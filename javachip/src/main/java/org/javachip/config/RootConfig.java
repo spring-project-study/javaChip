403b10cc6a1f6413bde2fc09005ff3ed347cd307
Seunghyun Yang <ggyool@naver.com>
2020-02-08 16:25:40 +0900

    -

--- javachip/src/main/java/org/javachip/config/RootConfig.java
+++ javachip/src/main/java/org/javachip/config/RootConfig.java
@@ -5,23 +5,30 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @MapperScan(basePackages= {"org.javachip.mapper"})
+@PropertySource("classpath:mysql.properties")
 public class RootConfig {
+	
+	@Autowired
+	private Environment env;
+	
 	@Bean
 	public DataSource dataSource() {
 		HikariConfig hikariConfig = new HikariConfig();
-		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
-		hikariConfig.setJdbcUrl("jdbc:log4jdbc:mysql://127.0.0.1:3306/javachipdb?serverTimezone=Asia/Seoul");
-		
-		hikariConfig.setUsername("ggyool");
-		hikariConfig.setPassword("ggyool123!@#");
+		hikariConfig.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+		hikariConfig.setJdbcUrl(env.getProperty("spring.datasource.url"));
+		hikariConfig.setUsername(env.getProperty("spring.datasource.username"));
+		hikariConfig.setPassword(env.getProperty("spring.datasource.password"));
 			
 		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 		return dataSource;
